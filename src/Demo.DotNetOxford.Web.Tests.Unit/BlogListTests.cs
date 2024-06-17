68646c5204013b283f523cee7bc3033a6b55edae
Neil Docherty <neil@jabbermouth.co.uk>
2024-06-17 16:30:13 +0100

    Initial test added + minimal component code

---
+++ src/Demo.DotNetOxford.Web.Tests.Unit/BlogListTests.cs
@@ -0,0 +1,29 @@
+﻿using Bunit;
+using Demo.DotNetOxford.Blog;
+using Demo.DotNetOxford.Web.Components.Shared;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+
+namespace Demo.DotNetOxford.Web.Tests.Unit;
+
+public class BlogListTests : TestContext
+{
+    [Fact]
+    public void OnRender_DisplayNoBlogsAvailable_WhenNoBlogs()
+    {
+        // Arrange
+        IEnumerable<BlogDto> blogs = [];
+        var blogRepository = Substitute.For<IBlogRepository>();
+        blogRepository.Posts().Returns(blogs);
+
+        Services.AddSingleton<IBlogRepository>(blogRepository);
+
+        // Act
+        var sut = RenderComponent<BlogList>();
+
+        // Assert
+        sut.FindAll("p").Count().Should().Be(1);
+        sut.Find("p").MarkupMatches("<p>No blogs available</p>");
+    }
+}
