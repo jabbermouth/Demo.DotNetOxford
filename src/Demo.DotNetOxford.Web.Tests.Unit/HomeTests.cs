b55f5201572b7ea4e066e1af1eafb8d1f8843443
Neil Docherty <neil@jabbermouth.co.uk>
2024-06-17 16:36:10 +0100

    Test to see if component is on home page

---
+++ src/Demo.DotNetOxford.Web.Tests.Unit/HomeTests.cs
@@ -0,0 +1,27 @@
+﻿using Bunit;
+using Demo.DotNetOxford.Blog;
+using Demo.DotNetOxford.Web.Components.Pages;
+using Demo.DotNetOxford.Web.Components.Shared;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+
+namespace Demo.DotNetOxford.Web.Tests.Unit;
+
+public class HomeTests : TestContext
+{
+    [Fact]
+    public void OnRender_DisplayBlogListComponent()
+    {
+        // Arrange
+        var blogRepository = Substitute.For<IBlogRepository>();
+
+        Services.AddSingleton<IBlogRepository>(blogRepository);
+
+        // Act
+        var sut = RenderComponent<Home>();
+
+        // Assert
+        sut.HasComponent<BlogList>().Should().BeTrue();
+    }
+}
