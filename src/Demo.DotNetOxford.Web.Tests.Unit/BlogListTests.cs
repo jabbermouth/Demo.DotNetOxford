d00225dedeb2369b227cdeeed74a7719799a9196
Neil Docherty <neil@jabbermouth.co.uk>
2024-06-17 16:32:10 +0100

    Add failing test for populating a blog list

--- src/Demo.DotNetOxford.Web.Tests.Unit/BlogListTests.cs
+++ src/Demo.DotNetOxford.Web.Tests.Unit/BlogListTests.cs
@@ -26,4 +26,28 @@ public class BlogListTests : TestContext
         sut.FindAll("p").Count().Should().Be(1);
         sut.Find("p").MarkupMatches("<p>No blogs available</p>");
     }
+
+    [Fact]
+    public void OnRender_DisplayBlogs_WhenBlogsAvailable()
+    {
+        // Arrange
+        IEnumerable<BlogDto> blogs = new[]
+        {
+            new BlogDto { Title = "Blog 1", Content = "Content for blog 1" },
+            new BlogDto { Title = "Blog 2", Content = "Content for blog 2" }
+        };
+        var blogRepository = Substitute.For<IBlogRepository>();
+        blogRepository.Posts().Returns(blogs);
+
+        Services.AddSingleton<IBlogRepository>(blogRepository);
+
+        // Act
+        var sut = RenderComponent<BlogList>();
+
+        // Assert
+        var divs = sut.FindAll("div");
+        divs.Count.Should().Be(2);
+        divs[0].MarkupMatches("<div><h2>Blog 1</h2><p>Content for blog 1</p></div>");
+        divs[1].MarkupMatches("<div><h2>Blog 2</h2><p>Content for blog 2</p></div>");
+    }
 }
